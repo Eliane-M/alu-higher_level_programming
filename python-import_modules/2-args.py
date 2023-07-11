aef61f8aa468c10d9af8409df540c669e3afad57
Eliane-M <munezeroeliane761@gmail.com>
2023-07-11 04:34:40 -0700

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,3 +11,7 @@ args = sys.argv[1:]
         print(f'{num_args} arguments{"s" if num_args > 1 else ""}:')
         for i, arg in enumerate(args):
             print(f"{i+1}: {arg}")
+
+
+if __name__ == '__main__':
+    main()
