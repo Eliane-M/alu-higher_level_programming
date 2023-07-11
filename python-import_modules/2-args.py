fc94ddcc77199b722d3a5fa33fa0beb771305e49
Eliane-M <munezeroeliane761@gmail.com>
2023-07-11 04:33:06 -0700

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f'{num_args} arguments{"s" if num_args > 1 else ""}:')
+        for i, arg in enumerate(args):
+            print(f"{i+1}: {arg}")
