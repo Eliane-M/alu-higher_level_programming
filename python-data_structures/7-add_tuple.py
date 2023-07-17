817ecb25377a42b16e7159cada68ce77d36a4864
Eliane-M <munezeroeliane761@gmail.com>
2023-07-17 01:58:14 -0700

    tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2] + (0, 0)[:2]
-
-    b1, b2 = tuple_b[:2] + (0, 0)[:2]
-
-    return (a1 + b1, a2 + b2)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    sum = (a[0] + b[0], a[1] + b[1])
+    return sum
