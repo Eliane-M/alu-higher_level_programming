ecdd3c4beccc862b6c375ab8f51309a383ff50a0
Eliane-M <munezeroeliane761@gmail.com>
2023-07-17 02:12:24 -0700

    8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def multiple_returns(sentence):
-
+    if len(sentence) == 0:
+        return None
+    else:
+        first_character = sentence[0]
+        return len(sentence), first_character
