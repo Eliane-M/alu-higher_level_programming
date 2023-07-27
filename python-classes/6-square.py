7a708e1c70d89a30dc3a4c437cc7013e85ca46bf
Eliane-M <munezeroeliane761@gmail.com>
2023-07-27 00:55:01 -0700

    squares

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,7 +40,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for - in range(self.__position[1]):
+            for _ in range(self.__position[1]):
                 print()
-            for - in range(self.__size):
+            for _ in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
