fbef32d191672aa3dbea98a4eabd02cc028c3b92
Eliane-M <munezeroeliane761@gmail.com>
2023-07-28 06:12:51 -0700

    inheritance

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+inheritance
+"""
+
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """
+    square classes
+    """
+    def __init__(self, size):
+        """The initialization method for
+        the class Square
+        """
+        self.__size = self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
