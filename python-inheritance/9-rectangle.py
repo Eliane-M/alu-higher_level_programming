1087d85930d0b99cd8847d2c1256dabbf358adb5
Eliane-M <munezeroeliane761@gmail.com>
2023-07-28 06:40:50 -0700

    inheritance

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -4,16 +4,40 @@ inheritance
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-class BaseGeometry()
-"""
+class BaseGeometry():
+    """
+    class BaseGeometry
+    """
+    def __init__(self, do_print=False):
+        if do_print:
+            self.count = 1
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        if not type(value) == int:
+            raise TypeError(f"{self.name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+        else:
+            return value
 
 
 class Rectangle(BaseGeometry):
     """
-    class
+    class rectangle
     """
     def __init__(self, width, height):
+        """
+        initialisation
+        """
         self.__width = self.integer_validator("width", width)
         self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+            return f"[Rectangle] {self.__width}/{self.__height}"
