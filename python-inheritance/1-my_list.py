8711b1c4eefb256b2225998689a120978e6dedc3
Eliane-M <munezeroeliane761@gmail.com>
2023-07-28 03:11:59 -0700

    inheritance

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+inheritance
+"""
+
+
+def print_sorted(self):
+    """
+    class
+    """
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
