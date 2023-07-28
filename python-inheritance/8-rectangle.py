89eecc2c183087ab9a0344a0d9b33df0cb364bd9
Eliane-M <munezeroeliane761@gmail.com>
2023-07-28 05:21:21 -0700

    inheritance

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+inheritance
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+basegeometry
+"""
+
+
+class Rectangle(Basegeometry):
+    """
+    class
+    """
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
