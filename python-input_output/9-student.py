6cf01bbfc9685c4659cfb7af2c1b0f26440df707
Eliane-M <munezeroeliane761@gmail.com>
2023-07-28 08:06:51 -0700

    input and ouput

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+input and ouput
+"""
+
+
+class Student:
+    """
+    class documentation
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialises
+        """
+        def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
