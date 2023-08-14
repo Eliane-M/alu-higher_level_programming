02ba31131770ee8bd8fb0388d569a62367d82dc9
Eliane-M <munezeroeliane761@gmail.com>
2023-08-14 02:01:30 -0700

    test driven

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,18 +5,18 @@ b is initially assigned to 98
 a and b must be integers
 """
 
+
 def add_integer(a, b=98):
     """
     if a or b are not integers raise a TypeError
     if a or b are float they should be first casted into an integer
     """
-    if not isinstance (a, (int, float)):
+    if not isinstance (a,(int, float)):
         raise TypeError('a must be an integer')
-    if not isinstance (b, (int, float)):
+    if not isinstance (b,(int, float)):
         raise TypeError('b must be an integer')
 
     a = int(a)
     b = int(b)
 
-    return (a + b)
-    
+    return (a + b) 
