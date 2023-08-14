b89da07bc54108f92288bca1a3fdbb6298d1a43a
Eliane-M <munezeroeliane761@gmail.com>
2023-08-14 04:13:47 -0700

    test driven

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,8 @@
 """
 divides all elements of a matrix
 """
+
+
 def matrix_divided(matrix, div):
     """
     matrix elements must be integers or floats otherwise raise a type error
@@ -10,9 +12,12 @@ def matrix_divided(matrix, div):
     div can't be 0
     raise a zerodivisionerroe if div == 0
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-    if any (len (row) != len(matrix[0]) for row in matrix):
+    if not isinstance(matrix, list) or
+            not all(isinstance(row, list) for row in matrix) or
+            not all(isinstance(element, int) or isinstance(element, float):
+        raise TypeError('matrix must be a matrix (list of lists)'
+                ' of integers/floats')
+    if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError('Each row of the matrix must have the same size')
     if not isinstance(div, (int, float)):
         raise TypeError('div must be a number')
---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Prints 'My name is' with the first_name and the second_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    both names should be strings
+    raise a type error if they are not strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string or last_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('first_name must be a string or last_name must be a string')
+    print ('My name is ' + first_name + last_name)
