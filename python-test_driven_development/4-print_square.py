e9425cc9e07061ab5518193019be8b88cd62eeb7
Eliane-M <munezeroeliane761@gmail.com>
2023-08-15 01:06:48 -0700

    test driven

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,9 +15,9 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
-    
+ 
     if size == 0:
-        print ()
+        print()
     else:
         for _ in range(size):
             print('#' * size)
