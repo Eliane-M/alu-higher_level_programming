e5969a640fadb3fec903763cec761e069e23ad09
Eliane-M <munezeroeliane761@gmail.com>
2023-08-16 20:49:27 -0700

    almost circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -34,7 +34,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        return self.x
+        return self.__x
 
     @x.setter
     def x(self, value):
@@ -42,7 +42,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        return self.y
+        return self.__y
 
     @y.setter
     def y(self, value):
