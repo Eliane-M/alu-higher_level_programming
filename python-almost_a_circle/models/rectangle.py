1bd4cee59b954fe809b8c1644a9365b7232872b7
Eliane-M <munezeroeliane761@gmail.com>
2023-08-16 21:11:56 -0700

    almost circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,10 @@ class Rectangle(Base):
     class attributes and methods
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        super id class
+        assign width, height, x, and y to right attributes
+        """
         super().__innit__(id)
         self.__width = width
         self.__height = height
@@ -18,32 +22,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        private instance for width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        setter for width
+        """
         self.__width = value
 
     @property
     def height(self):
+        """
+        height private instance
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        height setter
+        """
         self.__height = value
 
     @property
     def x(self):
+        """
+        private instance for x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        x setter
+        """
         self.__x = value
 
     @property
     def y(self):
+        """
+        private instance for y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        y setter
+        """
         self.__y = value
