f0eff6f361f8b96e5a9d505fc8e682f92daea2c3
Eliane-M <munezeroeliane761@gmail.com>
2023-08-16 21:27:40 -0700

    almost circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,6 +32,10 @@ class Rectangle(Base):
         """
         setter for width
         """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -46,6 +50,10 @@ class Rectangle(Base):
         """
         height setter
         """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if height <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
@@ -60,6 +68,10 @@ class Rectangle(Base):
         """
         x setter
         """
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        if x < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -74,4 +86,8 @@ class Rectangle(Base):
         """
         y setter
         """
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
