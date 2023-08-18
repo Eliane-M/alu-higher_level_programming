ca8fa8510ecae97b0c10ca14b32046c106962314
Eliane-M <munezeroeliane761@gmail.com>
2023-08-18 07:40:42 -0700

    almost circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,99 +1,123 @@
 #!/usr/bin/python3
-"""
-class rectangle that inherits from base
-"""
+""" rectangle module """
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    class attributes and methods
-    """
+    """ rectangle class """
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        super id class
-        assign width, height, x, and y to right attributes
-        """
-        super().__innit__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        """init general"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
-        """
-        private instance for width
-        """
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        setter for width
-        """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if width <= 0:
-            raise ValueError('width must be > 0')
+        """ width setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """
-        height private instance
-        """
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        height setter
-        """
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if height <= 0:
-            raise ValueError('height must be > 0')
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
-        """
-        private instance for x
-        """
+        """x getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """
-        x setter
-        """
-        if not isinstance(value, int):
-            raise TypeError('x must be an integer')
-        if x < 0:
-            raise ValueError('x must be >= 0')
+        """x setter"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """
-        private instance for y
-        """
+        """y getter"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-        y setter
-        """
-        if not isinstance(value, int):
-            raise TypeError('y must be an integer')
+        """ y setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError('y must be >= 0')
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
+        """ area cal """
+        return (self.__height * self.__width)
+
+    def display(self):
+        """ graphic representation """
+        print('\n' * self.__y, end='')
+        for i in range(self.__height):
+            print(' ' * self.__x + '#' * self.__width)
+
+    def __str__(self):
+        """ the string representation """
+        d = self.id
+        a = self.__x
+        b = self.__y
+        c = self.__width
+        e = self.__height
+        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".
+                format(d, a, b, c, e))
+
+    def update(self, *args, **kwargs):
+        """take multi update"""
+        dct = {}
+        if args is not None and len(args) > 0:
+            keys = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args) if len(args) <= 5 else 5):
+                dct[keys[i]] = args[i]
+        else:
+            dct = kwargs
+
+        if len(dct) > 0:
+            for key, value in dct.items():
+                if key == "id" and value is None:
+                    a = self.__width
+                    b = self.__height
+                    c = self.__x
+                    self.__init__(a, b, c, self.__y)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """just dictionary
+        representation
         """
-        public method for getting the area of the rectangle
-        """
-        return(self.__height * self.__weight)
+        dict = {}
+        dict["id"] = self.id
+        dict["width"] = self.width
+        dict["height"] = self.height
+        dict["x"] = self.x
+        dict["y"] = self.y
+        return (dict)
