860f54e3abc3d59cc3a468271cfb3019947054b8
Eliane-M <munezeroeliane761@gmail.com>
2023-10-02 13:16:00 -0700

    java

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (isNaN(a)) {
+    return (1);
+  }
+  if (a <= 0) {
+    return (1);
+  }
+  return a * factorial(a - 1);
+}
+
+const arg = process.argv[2];
+const inte = parseInt(arg);
+const result = factorial(inte);
+
+console.log(result);
