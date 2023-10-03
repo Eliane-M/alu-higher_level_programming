c5893b3a0a6462f41b081638fecd548f5c07c67e
Eliane-M <munezeroeliane761@gmail.com>
2023-10-03 05:10:13 -0700

    java

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args.length === 0 || args.length === 1) {
+  console.log('0');
+} else {
+  const integers = [...new Set(args.map(arg => parseInt(arg, 10)))];
+  const sortedIntegers = integers.sort((a, b) => b - a);
+  console.log(sortedIntegers[1]);
+}
