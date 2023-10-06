c1f1af8e76276c1b257be814018b700854b7b66f
Eliane-M <munezeroeliane761@gmail.com>
2023-10-05 23:44:47 -0700

    js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,13 +3,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.writeFile(filePath, string, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
-  }
-  if (string) {
-    console.log(string);
   }
 });
