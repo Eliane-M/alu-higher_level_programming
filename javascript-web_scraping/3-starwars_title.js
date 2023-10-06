2e4d416d69bf698ce63c6fad8135188a1a69fecf
Eliane-M <munezeroeliane761@gmail.com>
2023-10-06 02:53:18 -0700

    js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,14 +3,14 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const apiURL = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
 
 if (!movieId) {
   console.error('provide URL');
   process.exit(1);
 }
 
-request(apiURL, (err, response, body) => {
+const apiURL = 'https://swapi-api.alx-tools.com/api/films/';
+request(apiURL, function (err, response, body) {
   if (err) {
     console.error(err);
     return;
