bdb6e696fb433176ddcaa11a68a731778d581963
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2023-10-08 23:54:56 +0200

    js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const args = process.argv;
-
-request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    const charEnd = data.characters;
-    const loopTime = charEnd.length;
-    for (let i = 0; i < loopTime; i++) {
-      request(charEnd[i], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
           console.log(JSON.parse(body).name);
         }
       });
-    }
+    });
   }
 });
