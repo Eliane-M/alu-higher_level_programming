60d21d2562c6c4d5b3c519822a2d19b2fd7d1fc7
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2023-10-08 12:31:40 +0200

    jq

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+<script>
+  $(document).ready(function () {
+    $("#toggle_header").click(function () {
+      if ($("header").hasClass("red")) {
+        $("header").removeClass("red");
+        $("header").addClass("green");
+      } else {
+        $("header").removeClass("green");
+        $("header").addClass("red");
+      }
+    });
+  });
+</script>
